02a4fb7b4e3d95e0ac4fe40e95173a40275017d6
Manu-code-97 <emmanuilmyaun.pro@gmail.com>
2024-11-27 15:10:40 +0100

    update souscategorie entity

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategorieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategorieRepository::class)]
@@ -13,8 +15,49 @@ class Categorie
     #[ORM\Column]
     private ?int $id = null;
 
+    /**
+     * @var Collection<int, SousCategorie>
+     */
+    #[ORM\OneToMany(targetEntity: SousCategorie::class, mappedBy: 'categorie', orphanRemoval: true)]
+    private Collection $sousCategories;
+
+    public function __construct()
+    {
+        $this->sousCategories = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return Collection<int, SousCategorie>
+     */
+    public function getSousCategories(): Collection
+    {
+        return $this->sousCategories;
+    }
+
+    public function addSousCategory(SousCategorie $sousCategory): static
+    {
+        if (!$this->sousCategories->contains($sousCategory)) {
+            $this->sousCategories->add($sousCategory);
+            $sousCategory->setCategorie($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSousCategory(SousCategorie $sousCategory): static
+    {
+        if ($this->sousCategories->removeElement($sousCategory)) {
+            // set the owning side to null (unless already changed)
+            if ($sousCategory->getCategorie() === $this) {
+                $sousCategory->setCategorie(null);
+            }
+        }
+
+        return $this;
     }
 }
--- src/Entity/SousCategorie.php
+++ src/Entity/SousCategorie.php
@@ -13,8 +13,39 @@ class SousCategorie
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 40)]
+    private ?string $nom = null;
+
+    #[ORM\ManyToOne(inversedBy: 'sousCategories')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Categorie $categorie = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?Categorie
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?Categorie $categorie): static
+    {
+        $this->categorie = $categorie;
+
+        return $this;
     }
 }
