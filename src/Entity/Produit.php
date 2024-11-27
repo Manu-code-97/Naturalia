3c63b6240caec5574fa08a7f0a30d015fe1ff0a3
Manu-code-97 <emmanuilmyaun.pro@gmail.com>
2024-11-27 16:19:41 +0100

    Update entities
    update fournisseur
    update produit

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\ProduitRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ProduitRepository::class)]
@@ -13,8 +14,173 @@ class Produit
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 30)]
+    private ?string $image = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 6, scale: 2)]
+    private ?string $prix = null;
+
+    #[ORM\Column]
+    private ?int $quantiteStock = null;
+
+    #[ORM\Column]
+    private ?bool $local = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $origine = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $marque = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $ingredient = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 5, scale: 2)]
+    private ?string $poids = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 6, scale: 2, nullable: true)]
+    private ?string $prixPromo = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(string $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getQuantiteStock(): ?int
+    {
+        return $this->quantiteStock;
+    }
+
+    public function setQuantiteStock(int $quantiteStock): static
+    {
+        $this->quantiteStock = $quantiteStock;
+
+        return $this;
+    }
+
+    public function isLocal(): ?bool
+    {
+        return $this->local;
+    }
+
+    public function setLocal(bool $local): static
+    {
+        $this->local = $local;
+
+        return $this;
+    }
+
+    public function getOrigine(): ?string
+    {
+        return $this->origine;
+    }
+
+    public function setOrigine(string $origine): static
+    {
+        $this->origine = $origine;
+
+        return $this;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->marque;
+    }
+
+    public function setMarque(string $marque): static
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+
+    public function getIngredient(): ?string
+    {
+        return $this->ingredient;
+    }
+
+    public function setIngredient(?string $ingredient): static
+    {
+        $this->ingredient = $ingredient;
+
+        return $this;
+    }
+
+    public function getPoids(): ?string
+    {
+        return $this->poids;
+    }
+
+    public function setPoids(string $poids): static
+    {
+        $this->poids = $poids;
+
+        return $this;
+    }
+
+    public function getPrixPromo(): ?string
+    {
+        return $this->prixPromo;
+    }
+
+    public function setPrixPromo(?string $prixPromo): static
+    {
+        $this->prixPromo = $prixPromo;
+
+        return $this;
     }
 }
