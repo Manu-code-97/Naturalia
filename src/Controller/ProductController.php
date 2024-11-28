402af258a0276a9988567bbb1a98c17f6c841110
Kyross143 <kumarnath.samy@gmail.com>
2024-11-28 14:47:22 +0100

    Add product controller

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,15 +10,23 @@ use Symfony\Component\Routing\Attribute\Route;
 class ProductController extends AbstractController
 {
     #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    public function index(ProduitRepository $produitRepository): Response
     {
+        $produits = $produitRepository->findAll();
+
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'produits' => $produits,
+            
         ]);
     }
 
+    #[Route('/product/{produit}', name: 'app_produit')]
+    public function produit(ProduitRepository $produitRepository, $produit): Response
+    {
+        $produit = $produitRepository->find($produit);
 
-
-
-    
+        return $this->render('product/show.html.twig', [
+            'produit' => $produit,
+        ]);
+    }
 }
