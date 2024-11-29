145dc677706d25c908112d551d1452eac5c7c449
Manu-code-97 <emmanuilmyaun.pro@gmail.com>
2024-11-29 16:08:24 +0100

    Update fixtures
    Product
    SousCategorie
    Categorie

--- src/DataFixtures/Product.php
+++ src/DataFixtures/Product.php
@@ -5,33 +5,61 @@ namespace App\DataFixtures;
 use Faker\Factory as FakerFactory;
 
 use App\Entity\Produit;
+use App\Entity\Categorie;
+use App\Entity\SousCategorie;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class Product extends Fixture
 {
 
+    private array $categories = [];
+    private array $sousCategories = [];
+
     public function load(ObjectManager $manager): void
     {
         $faker = FakerFactory::create();
-        $produit = new Produit();
 
-        for ($i=0; $i < 100; $i++) { 
-            $produit = new Produit();
-            $produit->setImage('https://picsum.photos/200');
-            $produit->setNom($faker->sentence(3));
-            $produit->setDescription($faker->paragraph());
-            $produit->setPrix($faker->randomNumber(3, false));
-            $produit->setQuantiteStock($faker->randomNumber(4, false));
-            $produit->setLocal($faker->numberBetween(0, 1));
-            $produit->setOrigine($faker->paragraph());
-            $produit->setMarque($faker->paragraph());
-            $produit->setIngredient($faker->paragraph());
-            $produit->setPoids($faker->randomFloat(2, 0, 20));
-            $produit->setPrixPromo($faker->randomFloat(3, false));
-            $produit->setSlug($faker->randomFloat(3, false));
+        for ($i=1; $i <= 5; $i++){
+            $categorie = new Categorie();
+            $categorie->setNom('categorie'.$i);
+            $categorie->setSlug($categorie->getNom());
 
-            $manager->persist($produit);
+            $manager->persist($categorie);
+            array_push($this->categories, $categorie);
+        }
+
+        foreach ($this->categories as $categorie){
+            for ($i=0; $i < 3; $i++) { 
+                $sousCategorie = new SousCategorie();
+                $sousCategorie->setCategorie($categorie);
+                $sousCategorie->setNom($faker->sentence(2));
+                $sousCategorie->setSlug($sousCategorie->getNom());
+
+                $manager->persist($sousCategorie);
+                array_push($this->sousCategories, $sousCategorie);
+            }
+        }
+
+        foreach ($this->sousCategories as $sousCategorie){   
+            for ($i=0; $i < 50; $i++) { 
+                $produit = new Produit();
+                $produit->setImage('https://picsum.photos/200');
+                $produit->setNom($faker->sentence(3));
+                $produit->setDescription($faker->sentence(10));
+                $produit->setPrix($faker->randomNumber(3, false));
+                $produit->setQuantiteStock($faker->randomNumber(4, false));
+                $produit->setLocal($faker->numberBetween(0, 1));
+                $produit->setOrigine($faker->paragraph());
+                $produit->setMarque($faker->paragraph());
+                $produit->setIngredient($faker->paragraph());
+                $produit->setPoids($faker->randomFloat(2, 0, 20));
+                $produit->setPrixPromo((rand(0,6)==0)?$faker->randomFloat(2, 1, 100):null);
+                $produit->setSlug($produit->getNom());
+                $produit->setSousCategorie($sousCategorie);
+                
+                $manager->persist($produit);
+            }
         }
 
         $manager->flush();
