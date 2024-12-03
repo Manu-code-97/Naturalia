157c70084e24a8c7c096819effcf36ca573adb1a
DamiMa13 <damien.marques99@gmail.com>
2024-12-03 11:53:36 +0100

    Update ProductController pour détail produit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,15 +11,14 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ProductController extends AbstractController 
 { 
-    #[Route('/product', name: 'app_product')] 
-    public function showProduct(ProduitRepository $repo): Response { 
+    // #[Route('/product', name: 'app_product')] 
+    // public function showProduct(ProduitRepository $repo): Response { 
         
-        $products = $repo->findAll(); 
-        return $this->render('product/index.html.twig', 
-        [ 'products' => $products, ]); 
-    } 
-    
-    #[Route('/{category}/{sousCategory}/{product}', name: 'app_sous_category_products')]
+    //     $products = $repo->findAll(); 
+    //     return $this->render('product/index.html.twig', 
+    //     [ 'products' => $products, ]); 
+    // } 
+    #[Route('/{category}/{sousCategory}/', name: 'sousCatProduits')]
     public function sousCategory (ProduitRepository $repo , $sousCategory): Response{
         $products = $repo -> findProductsBySousCategory($sousCategory);
         $productsPromos = $repo -> getProductsOnPromotion();
@@ -29,13 +28,27 @@ class ProductController extends AbstractController
         'productsPromos' => $productsPromos,  ]); 
     }
 
+   
+    #[Route('/{category}/{sousCategory}/{product}', name: 'detailProduit')]
+    public function detailProduit (ProduitRepository $repo , $product): Response{
+        $product = $repo -> find($product);
+        // $productsSelection = $repo -> getProductsOnPromotion(); // a voir pour la selection
+        //dd ($products);
+        return $this->render('product/detail.html.twig', 
+        [ 'productDetail' => $product, 
+        //'productsPromos' => $productsPromos, 
+     ]); 
+    }
+
     #[Route('/products', name: 'product_list')]
     public function list(ProduitRepository $repo, PaginatorInterface $paginator, Request $request): Response 
     {
-        $queryBuilder = $repo->createQueryBuilder('p');
+        //$queryBuilder = $repo->createQueryBuilder('p');
+        $products = $repo->findAll(); 
 
         $pagination = $paginator->paginate(
-            $queryBuilder,
+            //$queryBuilder,
+            $products,
             $request->query->getInt('page ', 1 ),
             10
         );
