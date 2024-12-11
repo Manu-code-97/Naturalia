52b734031133265a05846bba5d9952a255b15b29
Youssef7594 <Yousseffaras75020@gmail.com>
2024-12-11 17:13:25 +0100

    Update check-out
     add panier.html.twig

--- src/Controller/CheckOutController.php
+++ src/Controller/CheckOutController.php
@@ -5,29 +5,38 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class CheckOutController extends AbstractController
 {
-    #[Route('/check/out', name: 'app_check_out')]
+    #[Route('/panier', name: 'app_check_out')]
     public function index(): Response
     {
         return $this->render('checkout/index.html.twig', [
             'controller_name' => 'CheckOutController',
         ]);
     }
-
-
-    #[Route('/panier/paiement', name: 'app_panier_paiement')]
-    public function panierPaiement(): Response
+    
+    #[Route('/panier/livraison', name: 'app_panier_paiement')]
+    public function panierlivraison(): Response
     {
         return $this->render('checkout/delivery.html.twig', [
             'controller_name' => 'CheckOutController',
         ]);
     }
 
+    #[Route('/panier/paiement', name: 'app_panier_paiement')]
+    #[IsGranted('ROLE_USER')]
+    public function panierPaiement(): Response
+    {
+        return $this->render('checkout/paiementDelivery.html.twig', [
+            'controller_name' => 'CheckOutController',
+        ]);
+    }
 
 
     #[Route('/confirmation', name: 'app_confirmation')]
+    #[IsGranted('ROLE_USER')]
     public function confirmation(): Response
     {
         return $this->render('checkout/confirmation.html.twig', [
