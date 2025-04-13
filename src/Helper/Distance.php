46e107e03cecc3eaa4b14742c583ede6510a3f80
Manu-code-97 <emmanuilmyaun.pro@gmail.com>
2025-04-13 21:12:13 +0200

    Remove filter from "categorie" + update "Distance.php" comments

--- src/Helper/Distance.php
+++ src/Helper/Distance.php
@@ -31,13 +31,13 @@ class Distance
         $difLatRad = ($this->latOrigin - $this->latDest) * M_PI / 180;
         $difLonRad = ($this->lonOrigin - $this->lonDest) * M_PI / 180;
 
-        // moitié du carré de la longueur de la corde
+        // Calcul de la valeur intermédiaire "a" selon la formule de Haversine
         $a = (sin($difLatRad/2) * sin($difLatRad/2)) + (cos($latDestRad) * cos($latOriginRad) * sin($difLonRad/2) * sin($difLonRad));
 
-        // distance angulaire
+        // Calcul de l'angle central (distance angulaire) entre les deux points en radians
         $c = 2 * atan2(sqrt($a), sqrt(1-$a));
 
-        // distance réelle
+        // Calcul de la distance réelle entre les deux points, arrondie à 2 décimales
         $distance = round($this->radius * $c, 2);
 
         return $distance;
